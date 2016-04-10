8d384cd86fcaeed40fc765d73cd6c05518f88c35
Vyacheslav <Shukmanv@inbox.ru>
2016-04-10 21:16:09 +0300

    added Task01

---
+++ Practice/src/start/Task01.java
@@ -0,0 +1,39 @@
+package start;
+
+public class Task01 {
+	
+	public static void main(String[] args) {
+		switch (3){
+			case 0:
+				System.out.println("ноль");
+				break;
+			case 1:
+				System.out.println("один");
+				break;
+			case 2:
+				System.out.println("два");
+				break;
+			case 3:
+				System.out.println("три");
+				break;
+			case 4:
+				System.out.println("четыре");
+				break;
+			case 5:
+				System.out.println("пять");
+				break;
+			case 6:
+				System.out.println("шесть");
+				break;
+			case 7:
+				System.out.println("семь");
+				break;
+			case 8:
+				System.out.println("восемь");
+				break;
+			case 9:
+				System.out.println("девять");
+				break;
+		}	
+	}
+}
