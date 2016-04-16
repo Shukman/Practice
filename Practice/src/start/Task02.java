45037291b55dede238bba17b00f4123b7762f95e
Vyacheslav <Shukmanv@inbox.ru>
2016-04-16 10:19:43 +0300

    added Task2

---
+++ Practice/src/start/Task02.java
@@ -0,0 +1,23 @@
+package start;
+
+public class Task02 {
+	
+	public static void main(String[] args) {
+		double x = 0;
+		
+		double a = 5;
+		double b = 4;
+		double c = 3;
+		double f = (x*x*a) + (b*x) + c;
+		
+		double e = -100;
+		double d = 100;
+		double g = (e + d) / 2;
+				
+		if (e * d < 0) {
+			System.out.println(e = g);
+		} else { System.out.println(d = g);
+		
+		}
+	}
+}
